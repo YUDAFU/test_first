2aee3ee670063916bfd1611ebf4f91324549947f
YUDAFU <2453366841@qq.com>
2018-09-07 17:27:27 +0800

    Update nj_jyl_chip.cpp
    descriptions about products

--- nj_jyl_chip.cpp
+++ nj_jyl_chip.cpp
@@ -1,14 +1,26 @@
 
+
 /****************************************************
 *
 * file name: blzn-bl606a0-chip
-* description: dual cortex-m4fp + RT-Thread + wifi chip
+* description: cortex-m4fp dual kernel + RT-Thread + wifi module
 * author: yljia
 * company: blzn
 * date:   20180816
-*
+* addr:   nanjing
 ****************************************************/
 //
-//wireless:wifi/BLE/Zigbee/
+//wireless:wifi/BLE/Zigbee/NFC
 //
+//门锁
+//无人机
+//
+//图传：流媒体, RTP&RTSP, Camera, DVP格式, YUV格式, MJPEG, FFMPEG
+//
+//
+//
+//i2c
+//spi
+//sdio
+//uart
 //
