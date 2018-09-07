101d08925e028b8e5ff524080a9aa993e4e2d1ec
jyl <china_cpu@163.com>
2018-09-07 17:00:17 +0800

    some descriptions about file-num004.cpp and file-num009.cpp

--- file-num004.cpp
+++ file-num004.cpp
@@ -1,4 +1,12 @@
 
 #file-num004.cpp
-#i2c:SDA, SCL
-#100K, 400K, 3.4M, 3.3M
+#
+#i2c:	SDA, SCL
+#clock:	100K, 400K, 3.4M, 3.3M
+#
+#
+#
+#
+#
+#
+#
--- file-num009.cpp
+++ file-num009.cpp
@@ -1,1 +1,13 @@
-99999999
+@99999999
+#
+#
+#
+#
+#
+#camera interface format: dvp yuv
+#i2c
+#BF2013 BYD
+#
+#date:		20180907
+#addr:		nanjing
+#compang:	blzn
